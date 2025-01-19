c90d162814758d97ee8cd27f4db2d7417901602d
liora_ves <lioravesnovaty@gmail.com>
2025-01-19 21:17:32 +0200

    bottom bar dynamics

--- lib/components/BottomNav.tsx
+++ lib/components/BottomNav.tsx
@@ -1,16 +1,21 @@
+"use client";
+
 import React from "react";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const BottomNav: React.FC = () => {
+  const pathname = usePathname();
+
   return (
     <nav className="bottom-nav">
-      <Link href="/newsDynamicTry" className="nav-item">
+      <Link href="/newsDynamicTry" className={`${"nav-item"} ${pathname === '/newsDynamicTry' ? "active" : ''}`}>
         עדכונים
       </Link>
-      <Link href="/wizard" className="nav-item">
+      <Link href="/wizard" className={`${"nav-item"} ${pathname === '/wizard' ? "active" : ''}`}>
         עזרה ראשונה
       </Link>
-      <Link href="/report" className="nav-item">
+      <Link href="/report" className={`${"nav-item"} ${pathname === '/report' ? "active" : ''}`}>
         דיווח
       </Link>
     </nav>
