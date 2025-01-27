004320ac1181e1147e771633de98a61402d30956
liora_ves <lioravesnovaty@gmail.com>
2025-01-28 00:41:34 +0200

    updated news and added share button to whatsapp

--- app/newsDynamic/page.tsx
+++ app/newsDynamic/page.tsx
@@ -2,6 +2,7 @@
 
 import { useEffect, useState } from "react";
 import styles from "./page.module.css";
+import ShareButton from "@/lib/components/ShareButton";
 
 interface NewsItem {
   title: string;
@@ -67,41 +68,29 @@ export default function News() {
     fetchNewsData();
   }, []);
 
+// Function to convert text with URLs into JSX
+const convertUrlsToLinks = (text: string) => {
+  const urlPattern = /https?:\/\/[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+(?:\/[^\s]*)?/g;
+  const websitePattern = /(?:www\.)?[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+(?:\/[^\s]*)?/g;
+
+  // Replace URLs with anchor elements
+  const parts = text.split(/\b(https?:\/\/[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+(?:\/[^\s]*)?|\b(?:www\.)?[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+(?:\/[^\s]*)?)/g);
+  
+  return parts.map((part, index) => {
+    if (urlPattern.test(part) || websitePattern.test(part)) {
+      // If part is a URL, return a link
+      return (
+        <a key={index} href={part.startsWith('http') ? part : `https://${part}`} target="_blank" rel="noopener noreferrer">
+          {part}
+        </a>
+      );
+    }
+    return part; // Otherwise, return the text as is
+  });
+};
+
   return (
     <main className={styles.mainContainer}>
-      <div className={styles.svgStyle}>
-        <svg
-          width="33"
-          height="36"
-          viewBox="0 0 33 36"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <g clip-path="url(#clip0_1438_244)">
-            <path
-              d="M19.0576 35.9999C18.0894 35.9999 17.1316 35.8512 16.2204 35.5432C13.316 34.5715 11.2036 32.2777 9.1586 30.0583C7.52258 28.2795 5.97457 26.6016 4.14699 25.6937C3.97097 25.6034 3.78976 25.5237 3.60338 25.4388C2.66111 25.0034 1.48587 24.4671 0.709278 23.2512C-0.34171 21.6158 -0.207101 19.1627 1.01992 17.5485C2.13821 16.0777 3.97614 15.3768 5.81408 15.7167C7.27407 15.9875 8.69265 16.9167 9.81612 18.3237L7.40868 20.3361C6.75634 19.5131 5.99011 18.9875 5.26011 18.8494C4.57153 18.722 3.86742 18.9768 3.46877 19.5025C3.05976 20.0441 2.98211 20.9945 3.3031 21.499C3.58267 21.9397 4.18323 22.2105 4.87181 22.5291C5.08408 22.6246 5.29635 22.7255 5.50344 22.8264C7.84357 23.9945 9.6608 25.9645 11.4159 27.8706C13.2745 29.8883 15.0296 31.7945 17.1834 32.5167C19.2491 33.2069 21.698 32.6866 23.7327 31.1255C25.6586 29.6494 27.2221 27.3184 28.2576 24.3822C29.1274 21.9291 29.6399 19.0034 29.8367 15.4512C29.9506 13.3114 29.9713 10.8317 29.205 8.68658L32.1199 7.59277C33.0777 10.2795 33.0725 13.168 32.9379 15.6317C32.7256 19.5025 32.1509 22.7202 31.1776 25.4706C29.935 28.991 28.0039 31.8264 25.5913 33.6795C23.6084 35.2034 21.2993 35.9999 19.0576 35.9999Z"
-              fill="#F38A56"
-            />
-            <path
-              d="M13.6991 20.0762C12.995 14.5169 9.81612 9.40889 5.19281 6.4142L6.84954 3.72217C12.2443 7.21597 15.9564 13.1788 16.7796 19.6673L13.6991 20.0762Z"
-              fill="#F38A56"
-            />
-            <path
-              d="M18.4104 18.0106C17.2714 12.085 14.5585 6.6 10.5616 2.16106L12.8448 0C17.23 4.87434 20.2069 10.8903 21.4546 17.3947L18.4052 18.0106H18.4104Z"
-              fill="#F38A56"
-            />
-            <path
-              d="M23.2356 16.6248C22.9353 11.9734 21.6669 7.33273 19.5598 3.20706L22.3141 1.73096C24.618 6.24954 26.0106 11.3256 26.3368 16.4124L23.2356 16.6248Z"
-              fill="#F38A56"
-            />
-          </g>
-          <defs>
-            <clipPath id="clip0_1438_244">
-              <rect width="33" height="36" fill="white" />
-            </clipPath>
-          </defs>
-        </svg>
-      </div>
       {loading ? (
         <div>
           <div style={{ height: "250px" }} />
@@ -111,54 +100,67 @@ export default function News() {
         </div>
       ) : (
         <div>
-          {/* Header Section */}
           <div className={styles.newsHeader}>
             <div className={styles.newsPageTitle}>עדכונים חמים</div>
             <a href="../report" className={styles.newsLink}>
-              <svg
-                width="24"
-                height="16"
-                viewBox="0 0 24 16"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M0.292892 7.29289C-0.0976315 7.68342 -0.0976315 8.31658 0.292892 8.70711L6.65685 15.0711C7.04738 15.4616 7.68054 15.4616 8.07107 15.0711C8.46159 14.6805 8.46159 14.0474 8.07107 13.6569L2.41421 8L8.07107 2.34315C8.46159 1.95262 8.46159 1.31946 8.07107 0.928932C7.68054 0.538408 7.04738 0.538408 6.65685 0.928932L0.292892 7.29289ZM24 7L1 7V9L24 9V7Z"
-                  fill="#F38F56"
-                />
-              </svg>
-              רוצה לעזור לנו להוסיף עדכון חם?
+            אם זיהית משהו מוזר ברשת שאתה חושב שחשוב לשתף כאן,{' '}
+            <span style={{ fontWeight: 600 }}>שלח לנו דיווח!</span>
             </a>
           </div>
-
           {/* Error Handling */}
           {error && <p className={styles.error}>{error}</p>}
 
-          {/* News List */}
           {newsData.map(
             ({ title, date, content, image }: NewsItem, i: number) => (
-              <div className={styles.newsCard} key={i}>
-                <p className={styles.newsDate}>{date}</p>
+            <div className={styles.newsCard} key={i}>
+              <div className={styles.titleWithArrow}>
                 <h2 className={styles.newsTitle}>{title}</h2>
+                {expandedIndex === i ? (
+                  <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg"  onClick={() => setExpandedIndex(null)}>
+                  <path d="M1.88924 15.2174L1.7136 14.9915L0.910667 14.1886C0.609568 13.8875 0.233193 13.5111 0.283376 13.21C0.33356 12.7584 0.684842 12.4573 1.1114 12.0307L4.82495 8.31718C5.07587 8.06626 5.07587 7.71498 4.82495 7.46407L0.459018 3.09814C0.183011 2.82213 0.0575527 2.5963 0.283377 2.21993C0.358652 2.14466 0.408835 2.04429 0.509202 1.94392L0.735025 1.76828L1.53796 0.965352C1.83905 0.664254 2.21543 0.287881 2.51653 0.338064C2.96817 0.388247 3.26927 0.739529 3.69583 1.16609L7.40938 4.87964C7.6603 5.13055 8.01158 5.13055 8.26249 4.87964L12.503 0.639163C12.8542 0.287881 13.2306 0.212606 13.6823 0.513704C13.7575 0.538795 13.8077 0.588979 13.9081 0.689345C13.9332 0.76462 13.9583 0.789711 14.0336 0.814803L15.1376 1.91883L15.3885 2.3203C15.539 2.52103 15.4638 2.79703 15.1627 3.14832L10.8469 7.46407C10.596 7.71498 10.596 8.06627 10.8469 8.31718L15.0874 12.5577C15.4387 12.9089 15.514 13.2853 15.2129 13.737C15.1878 13.8122 15.1376 13.8624 15.0372 13.9628C14.9619 13.9879 14.9368 14.013 14.9118 14.0882L13.8077 15.1923L13.4063 15.4432C13.2055 15.5937 12.9295 15.5185 12.5782 15.2174L8.26249 10.9016C8.01158 10.6507 7.6603 10.6507 7.40938 10.9016L3.04345 15.2675C2.76744 15.5436 2.54162 15.669 2.16524 15.4432C2.08997 15.3679 1.9896 15.3177 1.88924 15.2174Z" fill="#4E538A"/>
+                  </svg>
+
+                ):(
+                  <svg width="18" height="13" viewBox="0 0 18 13" fill="none" xmlns="http://www.w3.org/2000/svg" onClick={() => setExpandedIndex(i)}>
+                    <path d="M2 2L8.96053 10.9493C8.98055 10.975 9.01945 10.975 9.03947 10.9493L16 2" stroke="#4E538A" stroke-width="3.4"/>
+                  </svg>              
+                )}                  
+              </div>
+
+                <p className={styles.newsContent}>
+                  {expandedIndex === i
+                    ? convertUrlsToLinks(content)
+                    : `${convertUrlsToLinks(content.slice(0, 100))}...`}{" "}
+                     {expandedIndex === i ? (
+                  <span
+                    className={styles.readMore}
+                    onClick={() => setExpandedIndex(null)} // Collapse content
+                  >
+                  </span>
+                     ):(
+                      <span
+                      className={styles.readMore}
+                      onClick={() => setExpandedIndex(i)} // Expand content
+                    >
+                      קרא.י עוד
+                    </span>
+                  )}
+                </p>
                 {image && (
                   <img className={styles.newsImage} src={image} alt={title} />
                 )}
-                <p className={styles.newsContent}>
-                  {expandedIndex === i
-                    ? content
-                    : `${content.slice(0, 100)}...`}{" "}
-                  <span
-                    className={styles.readMore}
-                    onClick={() =>
-                      setExpandedIndex(expandedIndex === i ? null : i)
-                    }
-                  >
-                    {expandedIndex === i ? "הצג פחות" : "קרא עוד"}
-                  </span>
-                </p>
+                {expandedIndex === i && (
+                  <div className={styles.newsDateWrapper}>
+                    <p className={styles.newsDate}>{date}</p>
+                    <span className={styles.publishedBy}>פורסם על ידי: מערכת האיגוד</span>
+                    <ShareButton title={title}  content={content} newsLink={"https://product-jam-final-project.vercel.app/newsDynamic"} />
+                  </div>
+                )}
               </div>
             )
           )}
+          
+          <div style={{ marginBottom:"70px" }} />
         </div>
       )}
     </main>
