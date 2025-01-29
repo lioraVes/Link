cf300bfae348d45ea00f51c78f5578bda1cd4c97
liora_ves <lioravesnovaty@gmail.com>
2025-01-29 14:06:40 +0200

    opening fix

--- app/Opening.tsx
+++ app/Opening.tsx
@@ -1,11 +1,16 @@
-"use client";
-
 import React, { useState } from "react";
 import Lottie from "react-lottie-player";
 import openingData from "@/lib/animations/opening_screen.json";
 import hand from "@/lib/animations/hand.json";
 
 const OpeningAnimation = ({ onComplete }: { onComplete: () => void }) => {
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  if (!isClient) {
+    return null;
+  }
   return (
     <div style={{ width: "100%", height: "100%" }}>
       <Lottie
